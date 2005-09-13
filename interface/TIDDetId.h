cff46125d6f717545a63337ff711c4d60ad08cbd
cmsbuild <>
2005-09-13 16:10:11 +0000

    This commit was manufactured by cvs2git to create tag 'DataFormats- SiStripDetId-CMSSW_0_1_0'.
    Sprout from master 2005-08-09 08:35:16 UTC fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch> 'set of classes for the Tracker Strip DetId'
    Cherrypick from master 2005-09-13 16:10:09 UTC fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch> 'change method name':
        interface/TIDDetId.h

--- interface/TIDDetId.h
+++ interface/TIDDetId.h
@@ -22,14 +22,14 @@ namespace cms
     /**Construct from generic DetId */
     TIDDetId(const DetId& id); 
  
-    TIDDetId(uint32_t pos_neg,
-	     uint32_t whell,
+    TIDDetId(uint32_t side,
+	     uint32_t wheel,
 	     uint32_t ring,
 	     uint32_t det_fw_bw,
 	     uint32_t det,
 	     uint32_t ster) : DetId(cms::DetId::Tracker,StripSubdetector::TID){
-      id_ |= (pos_neg& pos_negMask_)      << pos_negStartBit_    |
-	     (whell& whellMask_)          << whellStartBit_      |
+      id_ |= (side& sideMask_)      << sideStartBit_    |
+	     (wheel& wheelMask_)          << wheelStartBit_      |
 	     (det_fw_bw& det_fw_bwMask_)  << det_fw_bwStartBit_  |
 	     (det& detMask_)              << detStartBit_        |
 	     (ster& sterMask_)            << sterStartBit_ ;
@@ -37,13 +37,17 @@ namespace cms
  
 
     /// positive or negative id
-    unsigned int posNeg() const{
-      return int((id_>>pos_negStartBit_) & pos_negMask_);
+    /**
+     * side() = 1 The DetId identify a module in the negative part
+     * side() = 2 The DetId identify a module in the positive part
+     */
+    unsigned int side() const{
+      return int((id_>>sideStartBit_) & sideMask_);
     }
 
-    /// whell id
-    unsigned int whell() const{
-      return int((id_>>whellStartBit_) & whellMask_);
+    /// wheel id
+    unsigned int wheel() const{
+      return int((id_>>wheelStartBit_) & wheelMask_);
     }
 
     ///ring id
@@ -58,26 +62,67 @@ namespace cms
      */
     std::vector<unsigned int> det() const
       { std::vector<unsigned int> num;
-      num[0]=((id_>>det_fw_bwStartBit_) & det_fw_bwMask_);
-      num[1]=((id_>>detStartBit_) & detMask_);
+      num.push_back(((id_>>det_fw_bwStartBit_) & det_fw_bwMask_));
+      num.push_back(((id_>>detStartBit_) & detMask_));
       return num ;}
 
-    /// stereo id
+    /// glued
+    /**
+     * glued() = 0 it's not a glued module
+     * glued() = 1 it's a glued module
+     */
+    unsigned int glued() const
+      {
+	if(((id_>>sterStartBit_)& sterMask_) == 1 ||
+	   ((id_>>sterStartBit_)& sterMask_) == 2){
+	  return 1;
+	}else{
+	return 0;
+	}
+      }
+
+    /// stereo 
+    /**
+     * stereo() = 0 it's not a stereo module
+     * stereo() = 1 it's a stereo module
+     */
     unsigned int stereo() const 
-      { return ((id_>>sterStartBit_)& sterMask_) ;}
+      {
+	if(((id_>>sterStartBit_)& sterMask_)==1){
+	  return ((id_>>sterStartBit_)& sterMask_);
+	}else{
+	return 0;
+	}
+      }
+
+    /**
+     * If the DetId identify a glued module return 
+     * the DetId of your partner otherwise return 0
+     */
+    unsigned int partnerDetId() const
+      {
+	if(((id_>>sterStartBit_)& sterMask_)==1){
+	  return (id_ + 1);
+	}else if(((id_>>sterStartBit_)& sterMask_)==2){
+	  return (id_ - 1);
+	}else{
+	  return 0;
+	}
+      }
+  
 
   private:
     /// two bits would be enough, but  we could use the number "0" as a wildcard
-    static const unsigned int pos_negStartBit_=   23;
-    static const unsigned int whellStartBit_=     16;
+    static const unsigned int sideStartBit_=   23;
+    static const unsigned int wheelStartBit_=     16;
     static const unsigned int ringStartBit_=       8;
     static const unsigned int det_fw_bwStartBit_=  7;
     static const unsigned int detStartBit_=        2;
     static const unsigned int sterStartBit_=       0;
     /// two bits would be enough, but  we could use the number "0" as a wildcard
 
-    static const unsigned int pos_negMask_=     0x3;
-    static const unsigned int whellMask_=       0xF;
+    static const unsigned int sideMask_=     0x3;
+    static const unsigned int wheelMask_=       0xF;
     static const unsigned int ringMask_=        0xFF;
     static const unsigned int det_fw_bwMask_=   0x1;
     static const unsigned int detMask_=         0x1F;
