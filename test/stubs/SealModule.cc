72865a08371c2ce248f24f99ea424c1541052abb
cmsbuild <>
2006-12-25 05:29:08 +0000

    This commit was manufactured by cvs2git to create tag 'DataFormats- SiStripDetId-CMSSW_1_3_1_HLT6'.
    Sprout from master 2006-10-30 20:46:29 UTC Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch> 'removed key classes and updated buildfile and classes.* files'
    Cherrypick from master 2006-12-25 05:28:53 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Fix BuildFile':
        BuildFile
    Delete:
        test/BuildFile
        test/stubs/SealModule.cc
        test/stubs/SiStripAnalyzeKeys.cc
        test/stubs/SiStripAnalyzeKeys.h

--- test/stubs/SealModule.cc
+++
@@ -1,7 +0,0 @@
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "PluginManager/ModuleDef.h"
-DEFINE_SEAL_MODULE();
-
-#include "DataFormats/SiStripDetId/test/stubs/SiStripAnalyzeKeys.h"
-DEFINE_ANOTHER_FWK_MODULE(SiStripAnalyzeKeys)
-
