b84b57e1ef5e452ad1046a8dd7dd8570164e771b
Harry Cheung <sha1-58e66f5ba2315a6f9cad0b2b2d41b73a4257a796@cern.ch>
2008-09-25 16:45:00 +0000

    For strawman A geometry

--- src/TOBDetId.cc
+++ src/TOBDetId.cc
@@ -10,7 +10,8 @@ TOBDetId::TOBDetId(const DetId& id) : SiStripDetId(id.rawId()) {
 bool TOBDetId::isDoubleSide() const {
   // Double Side: only layers 1 and 2
   if( this->glued() == 0 && ( this->layer() == 1 || this->layer() == 2 ) ) {
-    return true;
+//    return true;
+    return false;
   } else {
     return false;
   }
